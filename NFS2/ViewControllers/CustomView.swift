94495737b8bf1600dcbb88108072055b6c2add04
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-11-22 23:10:01 +0300

    Initial Commit

---
+++ NFS2/ViewControllers/CustomView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomView.swift
+//  NFS2
+//
+//  Created by mac on 23.11.2023.
+//
+
+import Foundation
---
+++ NFS2/ViewControllers/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFS2
+//
+//  Created by mac on 22.11.2023.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
