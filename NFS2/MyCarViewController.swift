414ba36d6c5cb5f6077265a1cf393ea947328396
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-11-28 19:19:37 +0300

    Only horizontal motion

--- NFS2/MyCarViewController.swift
+++ NFS2/MyCarViewController.swift
@@ -5,4 +5,29 @@
 //  Created by mac on 26.11.2023.
 //
 
-import Foundation
+import UIKit
+
+class CarViewController: UIViewController {
+    
+    private var carImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Создаем UIImageView и устанавливаем изображение автомобиля
+        let carImage = UIImage(named: "MyCar1")
+        carImageView = UIImageView(image: carImage)
+        carImageView.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+        view.addSubview(carImageView)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        // Анимация движения автомобиля
+        UIView.animate(withDuration: 1.0) {
+            self.carImageView.center.x += 100
+        }
+    }
+}
+
--- NFS2/ViewControllers/GameViewController.swift
+++ NFS2/ViewControllers/GameViewController.swift
@@ -6,18 +6,24 @@
 //
 
 import UIKit
+import CoreMotion
 
-class GameViewController: UIViewController {
-    
+class GameViewController: UIViewController, UICollisionBehaviorDelegate {
+
     private let gameCustomView = CustomViewGameScreen(frame: UIScreen.main.bounds)
     let soundController = SoundController.shared
-    
+    let collisionBehavior = UICollisionBehavior()
+    let carImageView = UIImageView()
+    let animator = UIDynamicAnimator()
+
     override func loadView() {
         view = gameCustomView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collisionBehavior.collisionDelegate = self
+
         view.backgroundColor = .gray
         
         let backButton = CustomBarButtonItem(title: "Назад", style: .plain, target: self, action: #selector(backAction))
@@ -26,23 +32,59 @@ class GameViewController: UIViewController {
         
         let roadView = RoadView(frame: view.bounds)
         self.view.addSubview(roadView)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        soundController.stopBackgroundMusic()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        soundController.startBackgroundMusic()
+        
+        let carImage = UIImage(named: "MyCar1")
+        carImageView.image = carImage
+        carImageView.frame = CGRect(x: view.bounds.midX - 50, y: view.bounds.height - 100, width: 150, height: 100)
+        self.view.addSubview(carImageView)
+        
+        animator.addBehavior(collisionBehavior)
+        
+        let carPhysicsBody = UIDynamicItemBehavior(items: [carImageView])
+        carPhysicsBody.allowsRotation = false
+        animator.addBehavior(carPhysicsBody)
+        
+        let roadBounds = CGRect(x: roadView.frame.origin.x, y: roadView.frame.origin.y, width: roadView.frame.width, height: roadView.frame.height - 50)
+        collisionBehavior.addBoundary(withIdentifier: "roadBoundary" as NSCopying, for: UIBezierPath(rect: roadBounds))
+        
+        collisionBehavior.collisionDelegate = self
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        view.addGestureRecognizer(panGesture)
     }
     
     @objc func backAction() {
-        // Воспроизведение звука при нажатии на кнопку "Назад"
-        if let backButton = navigationItem.leftBarButtonItem as? CustomBarButtonItem {
-            backButton.playButtonSound()
-        }
         navigationController?.popViewController(animated: true)
+        
+              if let backButton = navigationItem.leftBarButtonItem as? CustomBarButtonItem {
+                  backButton.playButtonSound()
+              }
+              navigationController?.popViewController(animated: true)
+          }
+    
+    
+    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        switch gesture.state {
+        case .began:
+            break
+        case .changed:
+            let newX = carImageView.frame.origin.x + translation.x
+           let newY = carImageView.frame.origin.y
+            
+            // Ограничить перемещение только по горизонтали
+                let maxX = view.bounds.width - carImageView.frame.width // Максимальная координата X
+                let newXClamped = min(max(0, newX), maxX)
+
+            carImageView.frame.origin = CGPoint(x: newXClamped, y: newY)
+            break
+        case .ended, .cancelled:
+            break
+        default:
+            break
+        }
+        
+        gesture.setTranslation(.zero, in: view)
     }
 }
